56f926f0bf36b4f4776e0f36b58248bdb296577b
austinkrueger <akruegr@gmail.com>
2023-06-09 15:13:07 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://austinkrueger.dev",
+  integrations: [tailwind(), image(), partytown(({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })), sitemap()]
+});
